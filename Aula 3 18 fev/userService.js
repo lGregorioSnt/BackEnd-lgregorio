eb7e463fe021bb580d37b4e513e3caaa81971c72
Lucas <Lucas.gregorio.lgs@gmail.com>
2025-02-18 11:22:26 -0300

    a

--- Aula 3 18 fev/userService.js
+++ Aula 3 18 fev/userService.js
@@ -2,17 +2,17 @@ const user = require("./user.js")
 
 class userService{
     constructor(user, nextid){
-        this.user = []; //array p armazena uzuario
+        this.users = []; //array p armazena uzuario
         this.nextid = 1; // contador p gerar id
         
     }
     addUser(nome,email){
         const user = new user(this.nextid++, nome, email)
-        this.user.push(user)
+        this.users.push(user)
         return user
     }
     getUsers(){
-        return this.user
+        return this.users
     }
 }
 
